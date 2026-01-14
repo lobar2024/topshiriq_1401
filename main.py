e70898bba17b0f076050c04611a1137f11b59d8b
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-01-14 22:09:17 +0500

    Add age classification based on user input

---
+++ main.py
@@ -0,0 +1,14 @@
+x = int(input('Yoshni kirit: '))
+
+if x <= 0:
+    print('Noto‘g‘ri yosh kiritildi')
+elif x <= 3:
+    print('Yosh bola')
+elif x < 7:
+    print('Bog‘cha yoshida')
+elif x <= 18:
+    print('Maktab yoshida')
+elif x < 100:
+    print('Katta yoshda va voyaga yetgan')
+else:
+    print('Juda katta yoshni  kiritdingiz')
